18385cf24d701b98875c716dff682695f301e8ed
Firane <gael.querharo@gmail.com>
2023-04-03 16:47:14 +0200

    - Affichage article html - Début product.js

---
+++ front/js/script.js
@@ -0,0 +1,33 @@
+let productsData = [];
+let productsContainer = document.getElementById('items');
+
+
+// fonction qui récupère toute la data depuis l'API
+async function fetchProducts() {
+    await fetch('http://localhost:3000/api/products')
+    .then((res) => res.json())
+    .then((data) => (productsData = data));
+
+    productsDisplay();
+}
+
+
+
+// Fonction qui génère l'article html pour chaque produits 
+function productsDisplay() {
+    productsContainer.innerHTML = productsData
+    .map(
+        (product) =>
+        `
+        <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+        </a>
+        `
+    )
+}
+
+window.addEventListener('load', fetchProducts);
